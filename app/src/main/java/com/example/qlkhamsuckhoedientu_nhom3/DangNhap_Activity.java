0095d0b1034bfe21731a8893d7d42406e59360b6
Tuan2210 <84191285+Tuan2210@users.noreply.github.com>
2022-04-27 10:42:11 +0700

    upload frm_DangKy

---
+++ app/src/main/java/com/example/qlkhamsuckhoedientu_nhom3/DangNhap_Activity.java
@@ -0,0 +1,14 @@
+package com.example.qlkhamsuckhoedientu_nhom3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DangNhap_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dangnhap);
+    }
+}
